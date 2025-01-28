b2825db8ab377c361525280a7995f5d21d76c402
Quang Dao <quangdao@umich.edu>
2025-01-28 11:45:54 -0500

    feat: overhaul `MlPoly`, add lots of TODOs

--- ZKLib/Data/MlPoly/Basic.lean
+++ ZKLib/Data/MlPoly/Basic.lean
@@ -4,174 +4,150 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Quang Dao
 -/
 
-import Mathlib.Algebra.MvPolynomial.Basic
-import Mathlib.Data.ZMod.Defs
-import Mathlib.Data.Nat.Log
-import Mathlib.Data.Matrix.Basic
-import Mathlib.Data.Fin.VecNotation
+-- import Mathlib.Data.Matrix.Mul
+import Mathlib.RingTheory.MvPolynomial.Basic
 
 /-!
   # Multilinear Polynomials
 
-  This file defines an efficient representation of multilinear polynomials, by their evaluations
-  on the Boolean hypercube `{0,1}^n`.
+  This file defines computable representations of **multilinear polynomials**.
 
-  Our operations are designed to be as efficient as possible.
+  The first representation is by their coefficients, and the second representation is by their
+  evaluations over the Boolean hypercube `{0,1}^n`. Both representations are defined as `Array`s of
+  size `2^n`, where `n` is the number of variables. We will define operations on these
+  representations, and prove equivalence between them, and with the standard Mathlib definition of
+  multilinear polynomials, which is the type `R⦃≤ 1⦄[X Fin n]` (notation for
+  `MvPolynomial.restrictDegree (Fin n) R 1`).
 -/
 
--- TODO: Make this the definition (and use coefficient basis by default)
+namespace Vector
+
+variable {R : Type*} [Mul R] [AddCommMonoid R] {n : ℕ}
+
+/-- Inner product between two vectors of the same size. Should be faster than `_root_.dotProduct`
+    due to efficient operations on `Array`s. -/
+def dotProduct (a b : Vector R n) : R :=
+  a.zipWith b (· * ·) |>.foldl (· + ·) 0
+
+scoped notation:80 a " *ᵥ " b => dotProduct a b
+
+#check List.Vector.casesOn
+
+/-! ### TODO: define induction principles for `Vector` similar to `List.Vector` -/
+
+theorem dotProduct_eq_matrix_dotProduct (a b : Vector R n) :
+    dotProduct a b = _root_.dotProduct a.get b.get := by
+  -- prove this by induction on `a` and `b`
+  sorry
+
+end Vector
+
 /-- `MlPoly n R` is the type of multilinear polynomials in `n` variables over a ring `R`. It is
-  represented by its coefficients as a `Batteries.Vector` of length `2^n`, i.e. an `Array` of size
-  `2^n`. We also do not put any algebraic structure on `R` to allow for more flexibility. -/
-def MlPoly'' (n : ℕ) (R : Type*) := Vector R (2 ^ n)
+    represented by its coefficients as an `Array` of size `2^n`. -/
+def MlPoly (R : Type*) (n : ℕ) := Vector R (2 ^ n)
 
--- We define multilinear polynomials over rings by their evaluation on the hypercube {0,1}^n (i.e.
--- the Lagrange basis).
-structure MlPoly (R : Type*) (n : ℕ) where
-  evals : Array R
-  hSize : evals.size = 2 ^ n
-deriving DecidableEq, Repr
+/-- `MlPolyEval n R` is the type of multilinear polynomials in `n` variables over a ring `R`. It is
+    represented by its evaluations over the Boolean hypercube `{0,1}^n`. -/
+def MlPolyEval (R : Type*) (n : ℕ) := Vector R (2 ^ n)
 
--- Alternative definition with coefficients in the monomial basis
-structure MlPoly' (R : Type*) (n : ℕ) where
-  coeffs : Array R
-  hSize : coeffs.size = 2 ^ n
-deriving DecidableEq, Repr
+variable {R : Type*} {n : ℕ}
 
+#check finFunctionFinEquiv
 
--- variable {R : Type} [DecidableEq R] [Inhabited R] [Ring R]
-
-section Math
-
-variable {R : Type} [Mul R] [AddCommMonoid R]
-
-/-- Inner product between two arrays. Do automatic truncation for arrays of different lengths. -/
-def Array.dotProduct (a b : Array R) : R :=
-  a.zip b |>.map (fun (a, b) => a * b) |>.foldl (· + ·) 0
-
-def Array.dotProduct' (a b : Array R) (hEq : a.size = b.size) : R := Array.dotProduct a b
-
-
-
--- @[simp]
--- lemma Array.dotProduct_eq_matrix_dotProduct (a b : Array R) (h : a.size = b.size) :
---     Array.dotProduct a b = Matrix.dotProduct a.get (h ▸ b.get) := sorry
-
-end Math
-
+#check Pi.single
 
 namespace MlPoly
 
-variable {R : Type} [DecidableEq R] [Inhabited R] [Ring R]
+/-! ### TODO: define `add`, `smul`, `nsmul`, `zsmul`, `eval₂`, `eval` -/
 
--- This function converts multilinear representation in the evaluation basis to the monomial basis
--- This is also called the Walsh-Hadamard transform (either that or the inverse)
+instance inhabited [Inhabited R] : Inhabited (MlPoly R n) := by simp [MlPoly]; infer_instance
 
-def walshHadamardTransform (a : Array R) (n : ℕ) (h : ℕ) : Array R :=
-  if h < n then
-    let a := (Array.range (2 ^ n)).foldl (fun a i =>
-      if i &&& (2 ^ h) == 0 then
-        let u := a.get! i
-        let v := a.get! (i + (2 ^ h))
-        (a.set! i (u + v)).set! (i + (2 ^ h)) (v - u)
-      else
-        a
-    ) a
-    walshHadamardTransform a n (h + 1)
-  else
-    a
-
-def evalToMonomial (a : Array R) : Array R := walshHadamardTransform a (Nat.clog 2 a.size) 0
-
-def invWalshHadamardTransform (a : Array R) (n : ℕ) (h : ℕ) : Array R :=
-  if h < n then
-    let a := (Array.range (2 ^ n)).foldl (fun a i =>
-      if i &&& (2 ^ h) == 0 then
-        let u := a.get! i
-        let v := a.get! (i + (2 ^ h))
-        (a.set! i (u + v)).set! (i + (2 ^ h)) (v - u)
-      else
-        a
-    ) a
-    invWalshHadamardTransform a n (h + 1)
-  else
-    a
-
-def monomialToEval (a : Array R) : Array R := invWalshHadamardTransform a (Nat.clog 2 a.size) 0
-
-@[simp]
-lemma evalToMonomial_size (a : Array R) : (evalToMonomial a).size = 2 ^ (Nat.clog 2 a.size) := by
-  unfold evalToMonomial
-  sorry
-
-
-def unitArray {R : Type} [Inhabited R] [Ring R] (n k : ℕ) : Array R :=
-  let initialArray : Array R := Array.mkArray n 0
-  initialArray.set! k 1
-
-variable [CommSemiring R] {n : ℕ}
-
-instance inhabited : Inhabited (MlPoly R n) :=
-  ⟨{ evals := Array.mkArray (2 ^ n) 0, hSize := by simp }⟩
-
--- Automatically pad to the next power of two
-def new (evals : Array R) : MlPoly R (Nat.clog 2 evals.size) :=
-  let n : ℕ := Nat.clog 2 evals.size -- upper log base 2
-  let padEvals : Array R := (Array.range (2 ^ n)).map
-    (fun i => if i < evals.size then evals.get! i else 0)
-  { evals := padEvals, hSize := by simp [padEvals, n] }
+-- Conform a list of coefficients to a `MlPoly` with a given number of variables
+-- May either pad with zeros or truncate
+def ofArray [Zero R] (coeffs : Array R) (n : ℕ): MlPoly R n :=
+  .ofFn (fun i => if h : i.1 < coeffs.size then coeffs.get i h else 0)
 
 -- Create a zero polynomial over n variables
-def newZero (n : ℕ) : MlPoly R n :=
-  { evals := Array.mkArray (2 ^ n) 0, hSize := by simp }
+def zero [Zero R] : MlPoly R n := ofArray (Array.mkArray (2 ^ n) 0) n
 
+/-- Add two `MlPoly`s -/
+def add [Add R] (p q : MlPoly R n) : MlPoly R n := p.zipWith q (· + ·)
+
+/-- Negation of a `MlPoly` -/
+def neg [Neg R] (p : MlPoly R n) : MlPoly R n := p.map (fun a => -a)
+
+/-- Scalar multiplication of a `MlPoly` -/
+def smul [Mul R] (r : R) (p : MlPoly R n) : MlPoly R n := p.map (fun a => r * a)
+
+/-- Scalar multiplication of a `MlPoly` by a natural number -/
+def nsmul [SMul ℕ R] (m : ℕ) (p : MlPoly R n) : MlPoly R n := p.map (fun a => m • a)
+
+/-- Scalar multiplication of a `MlPoly` by an integer -/
+def zsmul [SMul ℤ R] (m : ℤ) (p : MlPoly R n) : MlPoly R n := p.map (fun a => m • a)
+
+/-- TODO : fill out this instance -/
+instance [AddCommMonoid R] : AddCommMonoid (MlPoly R n) :=
+  {
+    add := add
+    add_assoc := by sorry
+    add_comm := by sorry
+    zero := zero
+    zero_add := by sorry
+    add_zero := by sorry
+    nsmul := nsmul
+    nsmul_zero := by sorry
+    nsmul_succ := by sorry
+  }
+
+/-- TODO : fill out this instance -/
+instance [Semiring R] : Module R (MlPoly R n) where
+  smul := smul
+  one_smul := by sorry
+  mul_smul := by sorry
+  smul_zero := by sorry
+  smul_add := by sorry
+  add_smul := by sorry
+  zero_smul := by sorry
 
 -- Generate the Lagrange basis for evaluation point r
 -- First, a helper function
-def lagrangeBasisAux (r : Array R) (evals : Array R) (ell : Nat) (j : Nat) (size : Nat) : Array R :=
-  if j >= ell then
-    evals
-  else
-    let size := size * 2
-    let evals :=
-      (Array.range size).reverse.foldl
-        (fun evals i =>
-          if i % 2 == 1 then
-            let scalar := evals.get! (i / 2)
-            let evals := evals.set! i (scalar * r.get! j)
-            let evals := evals.set! (i - 1) (scalar - evals.get! i)
-            evals
-          else evals)
-        evals
-    lagrangeBasisAux r evals ell (j + 1) size
-termination_by (ell - j)
+-- def lagrangeBasisAux (r : Array R) (evals : Array R) (ell : Nat) (j : Nat) (size : Nat) :
+--    Array R :=
+--   if j >= ell then
+--     evals
+--   else
+--     let size := size * 2
+--     let evals :=
+--       (Array.range size).reverse.foldl
+--         (fun evals i =>
+--           if i % 2 == 1 then
+--             let scalar := evals.get! (i / 2)
+--             let evals := evals.set! i (scalar * r.get! j)
+--             let evals := evals.set! (i - 1) (scalar - evals.get! i)
+--             evals
+--           else evals)
+--         evals
+--     lagrangeBasisAux r evals ell (j + 1) size
+-- termination_by (ell - j)
 
-def lagrangeBasis (r : Array R) : Array R :=
-  let ell := r.size
-  let evals := Array.mkArray (2 ^ ell) 1
-  lagrangeBasisAux r evals ell 0 1
+variable [CommSemiring R]
 
+/-- TODO: define this in a functional way -/
+def lagrangeBasis (r : Vector R n) : Vector R (2 ^ n) := sorry
+  -- let ell := r.size
+  -- let evals := Array.mkArray (2 ^ ell) 1
+  -- lagrangeBasisAux r evals ell 0 1
 
-def add (p q : MlPoly R n) : MlPoly R n :=
-  { evals := p.evals.zip q.evals |>.map (fun (a, b) => a + b),
-    hSize := by simp [p.hSize, q.hSize, Array.size_zip] }
+variable {S : Type*} [CommSemiring S]
 
+def map (f : R →+* S) (p : MlPoly R n) : MlPoly S n :=
+  Vector.map (fun a => f a) p
 
-def scalarMul (r : R) (p : MlPoly R n) : MlPoly R n :=
-  { evals := p.evals.map (fun a => r * a), hSize := by simp [p.hSize] }
+/-- Evaluate a `MlPoly` at a point -/
+def eval (p : MlPoly R n) (x : Vector R n) : R :=
+  Vector.dotProduct p (lagrangeBasis x)
 
--- Technically this is not the product of two multilinear polynomials, since the result of that
--- would no longer be multilinear. This is only defining the product of the evaluations.
-def mul (p q : MlPoly R n) : MlPoly R n :=
-  { evals := p.evals.zip q.evals |>.map (fun (a, b) => a * b),
-    hSize := by simp [p.hSize, q.hSize, Array.size_zip] }
-
-
-def eval (p : MlPoly R n) (x : Array R) (h : x.size = n) : R :=
-  Array.dotProduct p.evals (lagrangeBasis x)
-
--- Partially evaluate the polynomial at some variables
--- def bound_top_vars (p : MlPoly R) (x : Array R) : MlPoly R :=
+def eval₂ (p : MlPoly R n) (f : R →+* S) (x : Vector S n) : S := eval (map f p) x
 
 -- Theorems about evaluations
 
@@ -182,3 +158,49 @@ def eval (p : MlPoly R n) (x : Array R) (h : x.size = n) : R :=
 -- dotProduct simp [↓reduceIte, h] sorry
 
 end MlPoly
+
+section MlPolyEval
+
+-- TODO: define the functions below in a functional way (easier to prove theorems about)
+
+-- This function converts multilinear representation in the evaluation basis to the monomial basis
+-- This is also called the Walsh-Hadamard transform (either that or the inverse)
+
+-- def walshHadamardTransform (a : Array R) (n : ℕ) (h : ℕ) : Array R :=
+--   if h < n then
+--     let a := (Array.range (2 ^ n)).foldl (fun a i =>
+--       if i &&& (2 ^ h) == 0 then
+--         let u := a.get! i
+--         let v := a.get! (i + (2 ^ h))
+--         (a.set! i (u + v)).set! (i + (2 ^ h)) (v - u)
+--       else
+--         a
+--     ) a
+--     walshHadamardTransform a n (h + 1)
+--   else
+--     a
+
+-- def evalToMonomial (a : Array R) : Array R := walshHadamardTransform a (Nat.clog 2 a.size) 0
+
+-- def invWalshHadamardTransform (a : Array R) (n : ℕ) (h : ℕ) : Array R :=
+--   if h < n then
+--     let a := (Array.range (2 ^ n)).foldl (fun a i =>
+--       if i &&& (2 ^ h) == 0 then
+--         let u := a.get! i
+--         let v := a.get! (i + (2 ^ h))
+--         (a.set! i (u + v)).set! (i + (2 ^ h)) (v - u)
+--       else
+--         a
+--     ) a
+--     invWalshHadamardTransform a n (h + 1)
+--   else
+--     a
+
+-- def monomialToEval (a : Array R) : Array R := invWalshHadamardTransform a (Nat.clog 2 a.size) 0
+
+-- @[simp]
+-- lemma evalToMonomial_size (a : Array R) : (evalToMonomial a).size = 2 ^ (Nat.clog 2 a.size) := by
+--   unfold evalToMonomial
+--   sorry
+
+end MlPolyEval
