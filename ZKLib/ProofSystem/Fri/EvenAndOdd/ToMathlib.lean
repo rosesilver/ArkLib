308f79079cfa2a56a1a365068d6ae10710c97810
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-04-15 19:50:31 +0200

    coeffs_of_comp_minus_x and its dependencies

--- ZKLib/ProofSystem/Fri/EvenAndOdd/ToMathlib.lean
+++ ZKLib/ProofSystem/Fri/EvenAndOdd/ToMathlib.lean
@@ -10,51 +10,17 @@ variable (hChar : (2 : F) ≠ 0)
 private lemma coeffs_of_comp_minus_x_pos_degree {f : Polynomial F} {n : ℕ} (h : 0 < f.degree) :
     (f.comp (-Polynomial.X)).coeff n = if Even n then f.coeff n else -f.coeff n := by
   revert n
-  apply Polynomial.degree_pos_induction_on
-    (P := 
-      fun f 
-        => (∀ {n : ℕ}, 
-          (f.comp (-Polynomial.X)).coeff n
-            = if Even n then f.coeff n else -f.coeff n)) <;> try tauto 
-  · intro a ha n 
-    simp 
-    by_cases hpar : Even n <;> try simp [hpar]
-    rcases n with _ | n <;> try simp 
-    rcases n with _ | n
-    · simp at hpar 
-    · simp [Polynomial.coeff_X] 
-  · intro p hp ih n
-    simp 
-    rcases n with _ | n <;> try simp
-    simp [ih, Nat.even_add_one]
-    by_cases hpar : Even n <;> try simp [hpar]
-    · rw [←Nat.not_odd_iff_even] at hpar
-      simp [hpar]
-    · rw [Nat.not_even_iff_odd] at hpar 
-      simp [hpar]
-  · intro p a hp ih n 
-    simp [ih]
-    by_cases hpar : Even n <;> try simp [hpar]
-    rcases n with _ | n <;> try simp [hpar]
-    simp [Nat.even_iff] at hpar 
+  apply Polynomial.degree_pos_induction_on (h0 := h) (P := fun f => _)
+  · aesop (add simp Polynomial.coeff_X) 
+  · rintro _ _ _ (_ | _) <;> aesop (add simp [Nat.even_add_one, Nat.even_iff])
+  · rintro _ _ _ _ (_ | _) <;> aesop
     
 theorem coeffs_of_comp_minus_x {f : Polynomial F} {n : ℕ}  :
     (f.comp (-Polynomial.X)).coeff n = if Even n then f.coeff n else -f.coeff n := by
     by_cases hpos : 0 < f.degree 
     · rw [coeffs_of_comp_minus_x_pos_degree hpos]
-    · generalize hdeg : f.degree = d 
-      rcases d with _ | d 
-      · rw [WithBot.none_eq_bot, Polynomial.degree_eq_bot] at hdeg 
-        simp [hdeg]
-      · rw [WithBot.some_eq_coe] at hdeg 
-        rw [hdeg] at hpos
-        have hpos : d = 0 := by aesop 
-        have hdeg := Polynomial.natDegree_eq_of_degree_eq_some hdeg 
-        subst hpos
-        rw [Polynomial.natDegree_eq_zero] at hdeg 
-        rcases hdeg with ⟨x, hdeg⟩ 
-        rw [←hdeg]
-        rcases n with _ | n <;> try simp 
+    · have : f.natDegree = 0 := by aesop (add simp Polynomial.natDegree_pos_iff_degree_pos.symm)
+      cases n <;> aesop (add simp Polynomial.natDegree_eq_zero)
 
 private lemma comp_x_square_coeff_pos_deg {f : Polynomial F} {n : ℕ} (h : 0 < f.degree):
     (f.comp (Polynomial.X * Polynomial.X)).coeff n = if Even n then f.coeff (n.div2) else 0 := by
@@ -142,4 +108,3 @@ theorem comp_x_square_coeff {f : Polynomial F} {n : ℕ} :
           simp 
 
    
-
