e1787fe60091b6f1936590e8cecd4af65348d7dd
Katy Hristova <katerina.hristova@nethermind.io>
2025-04-30 16:42:12 +0100

    imports updated

--- ArkLib/Data/CodingTheory.lean
+++ ArkLib/Data/CodingTheory.lean
@@ -3,6 +3,7 @@ import ArkLib.Data.CodingTheory.DivergenceOfSets
 import ArkLib.Data.CodingTheory.InterleavedCodes
 import ArkLib.Data.CodingTheory.LinearCodes
 import ArkLib.Data.CodingTheory.ProximityGap
+import ArkLib.Data.CodingTheory.Prelims
 import ArkLib.Data.CodingTheory.ReedMuller
 import ArkLib.Data.CodingTheory.ReedSolomon
 import ArkLib.Data.CodingTheory.ReedSolomonCodes
