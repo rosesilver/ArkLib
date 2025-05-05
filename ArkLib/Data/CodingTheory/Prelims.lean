e8789ab8b6233135525b30236c1c254b2b89c2fc
Katy Hristova <katerina.hristova@nethermind.io>
2025-05-05 16:17:14 +0100

    preliminary notions

--- ArkLib/Data/CodingTheory/Prelims.lean
+++ ArkLib/Data/CodingTheory/Prelims.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Katerina Hristova, František Silváši
+-/
+
 import Mathlib.Algebra.Module.Submodule.Defs
 import Mathlib.Data.Matrix.Defs
 import Mathlib.Data.Matrix.RowCol
@@ -23,9 +29,15 @@ end Affine
 
 namespace Matrices
 
+/--
+the submodule spanned by the rows of an `κ x ι` matrix `U`.
+-/
 def rowSpan (U : Matrix (Fin κ) (Fin ι) F) : Submodule F (Fin ι → F) :=
   Submodule.span F {U i | i : Fin κ}
 
+/--
+the set of indices of columns where matrices `U` and `V` differ.
+-/
 def neqCols (U V : Matrix (Fin κ) (Fin ι) F) : Finset (Fin ι) :=
   {j | ∃ i : (Fin κ), V i j ≠ U i j}
 
