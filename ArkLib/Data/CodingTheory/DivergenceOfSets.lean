7a0f88be573456db9b89f99249ddae478ef73c1d
Katy Hristova <katerina.hristova@nethermind.io>
2025-05-05 16:02:11 +0100

    divergence of sets - final version

--- ArkLib/Data/CodingTheory/DivergenceOfSets.lean
+++ ArkLib/Data/CodingTheory/DivergenceOfSets.lean
@@ -1,4 +1,14 @@
+/-
+Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Katerina Hristova, František Silváši, Julian Sutherland
+-/
+
 import ArkLib.Data.CodingTheory.RelativeHammingDistance
+
+/-
+Proved definiton for divergence of an arbitrary set `U` from an arbitrary set `V`.
+-/
 
 namespace DivergenceOfSets
 
@@ -11,7 +21,7 @@ variable {ι : Type*} [Fintype ι]
          {U V C : Set (ι → F)}
 
 /--
-`d_U` is the set of achievable relative Hamming distances between elements `u ∈ U` and `V`
+`d_U` is the set of achievable relative Hamming distances between elements `u ∈ U` and `V`.
 -/
 def d_U [Nonempty ι] (U V : Set (ι → F)) : Set ℚ :=
   { d : ℚ | ∃ u ∈ U, δᵣ(u,V) = d }
@@ -26,7 +36,7 @@ lemma finite_d_U [Nonempty ι] : (d_U U V).Finite :=
   Set.Finite.subset RelativeHamming.finite_relHammingDistRange d_U_subset_relHammingDistRange
 
 /--
-divergence of the arbitrary set `U` to the arbitrary set `V`
+divergence of an arbitrary set `U` from an arbitrary set `V`.
 -/
 def div_U_from_V [Nonempty ι] (U : Set (ι → F)) (V : Set (ι → F)) : ℚ :=
   have : Fintype (d_U U V) := @Fintype.ofFinite _ finite_d_U
