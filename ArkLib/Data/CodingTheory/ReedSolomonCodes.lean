331f5525ba54db5c3b69bb2a08543fd9f9f0272d
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-05-07 14:07:44 +0200

    minDist - WIP

--- ArkLib/Data/CodingTheory/ReedSolomonCodes.lean
+++ ArkLib/Data/CodingTheory/ReedSolomonCodes.lean
@@ -204,8 +204,15 @@ lemma rate [Field F] {deg : ℕ} [NeZero deg] {α : Fin ι ↪ F} (h : deg ≤ �
 /--
   The minimal code distance of an RS code of length `ι` and dimensio `deg` is `ι - deg + 1`
 -/
-lemma minDist [Field F] {deg : ℕ} {α : Fin ι ↪ F} :
-  LinearCodes.minDist (ReedSolomon.code α deg) = ι - deg + 1 := by sorry
+lemma minDist [Field F] {deg : ℕ} {α : Fin ι ↪ F} [NeZero deg] (h : deg ≤ ι) :
+  LinearCodes.minDist (ReedSolomon.code α deg) = ι - deg + 1 := by
+  have : NeZero ι := by constructor; aesop
+  refine le_antisymm ?p₁ ?p₂
+  case p₁ => sorry
+  case p₂ =>
+    choose c hc using show ∃ c, c ∈ ReedSolomon.code α deg by sorry
+    let p := polynomialOfCoeffs c
+    sorry
 
 end ReedSolomonCode
 end
