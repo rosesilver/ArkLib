f48927ea0778f3b9aa9d0fe92ebb807d92134d30
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-05-20 16:31:51 +0100

    Add legal crap

--- ArkLib/Data/CodingTheory/BerlekampWelch/Condition.lean
+++ ArkLib/Data/CodingTheory/BerlekampWelch/Condition.lean
@@ -1,3 +1,8 @@
+/-
+Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: František Silváši, Ilia Vlasov
+-/
 import Init.Data.List.FinRange
 import Mathlib.Algebra.Field.Basic
 import Mathlib.Algebra.Polynomial.Basic
