2308944abc72dcf4799329cc0d9d95e3e6289edf
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-06-04 11:56:42 +0200

    WIP

--- ArkLib/Data/CodingTheory/ReedSolomon.lean
+++ ArkLib/Data/CodingTheory/ReedSolomon.lean
@@ -161,90 +161,90 @@ end Vandermonde
 
 namespace ReedSolomonCode
 
-lemma natDegree_lt_of_lbounded_zero_coeff [Semiring F] {p : F[X]} {deg : ℕ} [NeZero deg]
-  (h : ∀ i, deg ≤ i → p.coeff i = 0) : p.natDegree < deg := by
-  aesop (add unsafe [(by by_contra), (by specialize h p.natDegree)])
+-- lemma natDegree_lt_of_lbounded_zero_coeff [Semiring F] {p : F[X]} {deg : ℕ} [NeZero deg]
+--   (h : ∀ i, deg ≤ i → p.coeff i = 0) : p.natDegree < deg := by
+--   aesop (add unsafe [(by by_contra), (by specialize h p.natDegree)])
 
-def polynomialOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg] (coeffs : Fin deg → F) : F[X] :=
-  ⟨
-    Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0},
-    fun i ↦ if h : i < deg then coeffs ⟨i, h⟩ else 0,
-    fun a ↦ by aesop (add safe (by existsi a))
-                     (add simp [Fin.natCast_def, Nat.mod_eq_of_lt])
-  ⟩
+-- def polynomialOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg] (coeffs : Fin deg → F) : F[X] :=
+--   ⟨
+--     Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0},
+--     fun i ↦ if h : i < deg then coeffs ⟨i, h⟩ else 0,
+--     fun a ↦ by aesop (add safe (by existsi a))
+--                      (add simp [Fin.natCast_def, Nat.mod_eq_of_lt])
+--   ⟩
 
-def coeffsOfPolynomial [Semiring F] {deg : ℕ} [NeZero deg] (p : F[X]) : Fin deg → F :=
-  fun ⟨x, _⟩ ↦ p.coeff x
+-- def coeffsOfPolynomial [Semiring F] {deg : ℕ} [NeZero deg] (p : F[X]) : Fin deg → F :=
+--   fun ⟨x, _⟩ ↦ p.coeff x
 
-@[simp]
-lemma natDegree_polynomialOfCoeffs_deg_lt_deg
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  (polynomialOfCoeffs coeffs).natDegree < deg := by
-  aesop (add simp polynomialOfCoeffs)
-        (add safe apply natDegree_lt_of_lbounded_zero_coeff)
+-- @[simp]
+-- lemma natDegree_polynomialOfCoeffs_deg_lt_deg
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   (polynomialOfCoeffs coeffs).natDegree < deg := by
+--   aesop (add simp polynomialOfCoeffs)
+--         (add safe apply natDegree_lt_of_lbounded_zero_coeff)
 
-@[simp]
-lemma degree_polynomialOfCoeffs_deg_lt_deg
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  (polynomialOfCoeffs coeffs).degree < deg := by
-  aesop (add simp [polynomialOfCoeffs, degree_lt_iff_coeff_zero])
+-- @[simp]
+-- lemma degree_polynomialOfCoeffs_deg_lt_deg
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   (polynomialOfCoeffs coeffs).degree < deg := by
+--   aesop (add simp [polynomialOfCoeffs, degree_lt_iff_coeff_zero])
 
-@[simp]
-lemma coeff_polynomialOfCoeffs_eq_coeffs
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  Fin.liftF' (polynomialOfCoeffs coeffs).coeff = coeffs := by
-  aesop (add simp [Fin.liftF', polynomialOfCoeffs])
+-- @[simp]
+-- lemma coeff_polynomialOfCoeffs_eq_coeffs
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   Fin.liftF' (polynomialOfCoeffs coeffs).coeff = coeffs := by
+--   aesop (add simp [Fin.liftF', polynomialOfCoeffs])
 
-lemma coeff_polynomialOfCoeffs_eq_coeffs'
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  (polynomialOfCoeffs coeffs).coeff = fun x ↦ if h : x < deg then coeffs ⟨x, h⟩ else 0 := by
-  aesop (add simp polynomialOfCoeffs)
+-- lemma coeff_polynomialOfCoeffs_eq_coeffs'
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   (polynomialOfCoeffs coeffs).coeff = fun x ↦ if h : x < deg then coeffs ⟨x, h⟩ else 0 := by
+--   aesop (add simp polynomialOfCoeffs)
 
-@[simp]
-lemma coeff_polynomialOfCoeffs_eq_coeffs''
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  (polynomialOfCoeffs coeffs).coeff = Fin.liftF coeffs := by
-  aesop (add simp [Fin.liftF', polynomialOfCoeffs])
+-- @[simp]
+-- lemma coeff_polynomialOfCoeffs_eq_coeffs''
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   (polynomialOfCoeffs coeffs).coeff = Fin.liftF coeffs := by
+--   aesop (add simp [Fin.liftF', polynomialOfCoeffs])
 
-@[simp]
-lemma polynomialOfCoeffs_mem_degreeLT
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  polynomialOfCoeffs coeffs ∈ degreeLT F deg := by
-  aesop (add simp Polynomial.mem_degreeLT)
+-- @[simp]
+-- lemma polynomialOfCoeffs_mem_degreeLT
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   polynomialOfCoeffs coeffs ∈ degreeLT F deg := by
+--   aesop (add simp Polynomial.mem_degreeLT)
 
-@[simp]
-lemma polynomialOfCoeffs_eq_zero [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  polynomialOfCoeffs coeffs = 0 ↔ ∀ (x : ℕ) (h : x < deg), coeffs ⟨x, h⟩ = 0 := by
-  simp [polynomialOfCoeffs, AddMonoidAlgebra.ext_iff]
+-- @[simp]
+-- lemma polynomialOfCoeffs_eq_zero [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   polynomialOfCoeffs coeffs = 0 ↔ ∀ (x : ℕ) (h : x < deg), coeffs ⟨x, h⟩ = 0 := by
+--   simp [polynomialOfCoeffs, AddMonoidAlgebra.ext_iff]
 
-lemma polynomialOfCoeffs_coeffsOfPolynomial [Semiring F] {deg : ℕ} [NeZero deg] {p : F[X]}
-  (h : p.natDegree + 1 = deg) : polynomialOfCoeffs (coeffsOfPolynomial (deg := deg) p) = p := by
-  ext x; symm
-  aesop (add simp [polynomialOfCoeffs, coeffsOfPolynomial, coeff_polynomialOfCoeffs_eq_coeffs'])
-        (add safe apply coeff_eq_zero_of_natDegree_lt)
-        (add safe (by omega))
+-- lemma polynomialOfCoeffs_coeffsOfPolynomial [Semiring F] {deg : ℕ} [NeZero deg] {p : F[X]}
+--   (h : p.natDegree + 1 = deg) : polynomialOfCoeffs (coeffsOfPolynomial (deg := deg) p) = p := by
+--   ext x; symm
+--   aesop (add simp [polynomialOfCoeffs, coeffsOfPolynomial, coeff_polynomialOfCoeffs_eq_coeffs'])
+--         (add safe apply coeff_eq_zero_of_natDegree_lt)
+--         (add safe (by omega))
 
-@[simp]
-lemma coeffsOfPolynomial_polynomialOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg]
-  {coeffs : Fin deg → F} : coeffsOfPolynomial (polynomialOfCoeffs coeffs) = coeffs := by
-  ext x; symm
-  aesop (add simp [polynomialOfCoeffs, coeffsOfPolynomial, coeff_polynomialOfCoeffs_eq_coeffs'])
-        (add safe (by omega))
+-- @[simp]
+-- lemma coeffsOfPolynomial_polynomialOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg]
+--   {coeffs : Fin deg → F} : coeffsOfPolynomial (polynomialOfCoeffs coeffs) = coeffs := by
+--   ext x; symm
+--   aesop (add simp [polynomialOfCoeffs, coeffsOfPolynomial, coeff_polynomialOfCoeffs_eq_coeffs'])
+--         (add safe (by omega))
 
-@[simp]
-lemma support_polynomialOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
-  (polynomialOfCoeffs coeffs).support =
-  Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0} := rfl
+-- @[simp]
+-- lemma support_polynomialOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} :
+--   (polynomialOfCoeffs coeffs).support =
+--   Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0} := rfl
 
-@[simp]
-lemma eval_polynomialsOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} {α : F} :
-  (polynomialOfCoeffs coeffs).eval α = ∑ x ∈ {i | coeffs i ≠ 0}, coeffs x * α ^ x.1 := by
-  simp [eval_eq_sum, sum_def, coeff_polynomialOfCoeffs_eq_coeffs', Fin.liftF]
+-- @[simp]
+-- lemma eval_polynomialsOfCoeffs [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} {α : F} :
+--   (polynomialOfCoeffs coeffs).eval α = ∑ x ∈ {i | coeffs i ≠ 0}, coeffs x * α ^ x.1 := by
+--   simp [eval_eq_sum, sum_def, coeff_polynomialOfCoeffs_eq_coeffs', Fin.liftF]
 
-@[simp]
-lemma isRoot_polynomialsOfCoeffs
-  [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} {x : F} :
-  IsRoot (polynomialOfCoeffs coeffs) x ↔ eval x (polynomialOfCoeffs coeffs) = 0 := by rfl
+-- @[simp]
+-- lemma isRoot_polynomialsOfCoeffs
+--   [Semiring F] {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F} {x : F} :
+--   IsRoot (polynomialOfCoeffs coeffs) x ↔ eval x (polynomialOfCoeffs coeffs) = 0 := by rfl
 
 lemma natDegree_lt_of_mem_degreeLT
   [Semiring F] {deg : ℕ} [NeZero deg] {p : F[X]} (h : p ∈ degreeLT F deg) : p.natDegree < deg := by
