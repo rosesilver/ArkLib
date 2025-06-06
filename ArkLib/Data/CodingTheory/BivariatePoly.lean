7cb7d7bb831df6d8224bb045ca66156a2d197ce9
Katy Hristova <katherina.hristova@gmail.com>
2025-06-06 12:50:49 +0100

    new degreeX def

--- ArkLib/Data/CodingTheory/BivariatePoly.lean
+++ ArkLib/Data/CodingTheory/BivariatePoly.lean
@@ -22,114 +22,23 @@ variable {F : Type} [Semiring F]
          (P P₁ P₂ : Finset F) [Nonempty P] [Nonempty P₁] [Nonempty P₁]
          (a : F)
          (f g : F[X][Y])
-/--
-The vector of coefficients of a bivariate polynomial, where coefficients are interpreted as
-polynomials in the first variable `X`.
+
+-- def coeffsOfBivPolynomial (f : F[X][Y]) : Fin f.natDegree → F[X] :=
+-- fun ⟨y, _⟩ ↦ Polynomial.coeff f y
+
+/---
+The `X` polynomial which is a coeff of `Y^n`.
 -/
-def coeffsOfBivPolynomial (f : F[X][Y]) : Fin f.natDegree → F[X] :=
-fun ⟨y, _⟩ ↦ Polynomial.coeff f y
+def coeff_Y_n (f : F[X][Y]) : F[X] :=
+  f.coeff n
 
--- lemma natDegreeBiv_lt_of_lbounded_zero_coeff {f : F[X][Y]} {deg : ℕ} [NeZero deg]
---   (h : ∀ i, deg ≤ i → f.coeff i = 0) : f.natDegree < deg := by
---   aesop (add unsafe [(by by_contra), (by specialize h f.natDegree)])
+def coeffs (f : F[X][Y]) : Finset F[X] :=
+  f.support.image (fun n => f.coeff n)
 
--- /--
--- Construct a bivariate polynomial in `X` and `Y` from a vector of coefficients,
--- which are polynomials in `X`.
--- -/
--- def polynomialOfCoeffsOfBiv {deg : ℕ} [NeZero deg] (coeffs : Fin deg → F[X]) : F[X][Y] :=
---  ⟨
---     Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0},
---     fun i ↦ if h : i < deg then coeffs ⟨i, h⟩ else 0,
---     fun a ↦ by aesop (add safe (by existsi a))
---                      (add simp [Fin.natCast_def, Nat.mod_eq_of_lt])
---   ⟩
-
--- @[simp]
--- lemma natDegree_polynomialOfCoeffsOfBiv_deg_lt_deg {deg : ℕ} [NeZero deg]
--- (coeffs : Fin deg → F[X]) :
---   (polynomialOfCoeffsOfBiv coeffs).natDegree < deg := by
---   aesop (add simp polynomialOfCoeffsOfBiv)
---         (add safe apply natDegreeBiv_lt_of_lbounded_zero_coeff)
-
--- @[simp]
--- lemma degree_polynomialOfCoeffsOfBiv_deg_lt_deg
---   {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
---   (polynomialOfCoeffsOfBiv coeffs).degree < deg := by
---   aesop (add simp [polynomialOfCoeffsOfBiv, degree_lt_iff_coeff_zero])
-
--- @[simp]
--- lemma coeff_polynomialOfCoeffsOfBiv_eq_coeffs {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
---   (polynomialOfCoeffsOfBiv coeffs).coeff ∘ Fin.val = coeffs := by -- NOTE TO SELF: `liftF' coeffs`!
---   aesop (add simp polynomialOfCoeffsOfBiv)
-
--- lemma coeff_polynomialOfCoeffsOfBiv_eq_coeffs' {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
---   (polynomialOfCoeffsOfBiv coeffs).coeff = fun x ↦ if h : x < deg then coeffs ⟨x, h⟩ else 0 := by
---   aesop (add simp polynomialOfCoeffsOfBiv)
-
--- @[simp]
--- lemma polynomialOfCoeffsOfBiv_mem_degreeLT {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
---   polynomialOfCoeffsOfBiv coeffs ∈ degreeLT F[X] deg := by
---   aesop (add simp Polynomial.mem_degreeLT)
-
-
--- @[simp]
--- lemma polynomialOfCoeffsOfBiv_eq_zero {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
---   polynomialOfCoeffsOfBiv coeffs = 0 ↔ ∀ (x : ℕ) (h : x < deg), coeffs ⟨x, h⟩ = 0 := by
---   simp [polynomialOfCoeffsOfBiv, AddMonoidAlgebra.ext_iff]
-
--- lemma polynomialOfCoeffsBiv_coeffsOfPolynomialBiv (deg : ℕ) [NeZero deg]
---   {f : F[X][Y]} (h : f.natDegree + 1 = deg)
---   : polynomialOfCoeffsOfBiv (coeffsOfBivPolynomial deg f) = f := by sorry
-  -- ext x; symm
-  -- aesop (add simp [polynomialOfCoeffsOfBiv, coeffsOfBivPolynomial, coeff_polynomialOfCoeffsOfBiv_eq_coeffs'])
-  --       (add safe apply coeff_eq_zero_of_natDegree_lt)
-  --       (add safe (by omega))
-
-
--- @[simp]
--- lemma coeffsOfBivPolynomial_polynomialOfCoeffsOfBiv {deg : ℕ} [NeZero deg]
---   {coeffs : Fin deg → F[X]} : coeffsOfBivPolynomial deg (polynomialOfCoeffsOfBiv coeffs) = coeffs
---   := by
---   ext x; symm
---   aesop (add simp [
---       polynomialOfCoeffsOfBiv, coeffsOfBivPolynomial, coeff_polynomialOfCoeffsOfBiv_eq_coeffs'
---                    ])
---         (add safe (by omega))
-
--- @[simp]
--- lemma support_polynomialOfBivCoeffs (deg : ℕ) [NeZero deg] {coeffs : Fin deg → F[X]} :
---   (polynomialOfCoeffsOfBiv coeffs).support =
---   Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0} := rfl
-
-
-def evalAtX (a : F) (f : F[X][Y]) : F[X] :=
-  ⟨Finsupp.mapRange (Polynomial.eval a) eval_zero f.toFinsupp⟩
-
-lemma evalAtX_deg_le_degY (a : F) (f : F[X][Y]) :
-  (evalAtX a f).natDegree ≤ f.natDegree := by
-  sorry
-
-/--
-Evaluating a bivariate polynomial in the first variable `X` on a set of points resulting in a set of
-univariate polynomials in `Y`.
--/
-def evalAtXOnASet (P : Finset F) (f : F[X][Y]) : Set (Polynomial F) :=
- {h : Polynomial F | ∃ a ∈ P, evalAtX a f = h}
-
-/--
-The evaluation at a point of a bivariate polynomial in the second variable `Y`.
--/
-def evalAtY (a : F) (f : F [X][Y]) : Polynomial F :=
-  Polynomial.eval (Polynomial.C a) f
-
-/--
-Evaluating a bivariate polynomial in the second variable `Y` on a set of points resulting
-in a set of univariate polynomials in `X`.
--/
-def evalAtYOnASet (P : Finset F) (f : F[X][Y]) : Set (Polynomial F) :=
-  {h : Polynomial F | ∃ a ∈ P, evalAtY a f = h}
-
+--Katy new def of degree X
+--Katy TO DO: change sup to max' after I write a proof for f.toFinsupp.support.Nonempty
+def degreeX'' (f : F[X][Y]) : ℕ :=
+  f.toFinsupp.support.sup (fun n => (f.coeff n).natDegree)
 
 /-the next def, lemmma and def give an alternative def of degreeX. the proof of nonempty
 forces the condition of `f ≠ 0`. So this definition is closer to the degree rather than
@@ -162,6 +71,38 @@ def degreeX (f : F[X][Y]) : ℕ :=
 The `Y` degree of a bivariate polynomial.
 -/
 def degreeY (f : F[X][Y]) : ℕ := Polynomial.natDegree f
+
+def evalAtX (a : F) (f : F[X][Y]) : Polynomial F :=
+  ⟨Finsupp.mapRange (Polynomial.eval a) eval_zero f.toFinsupp⟩
+
+--Katy: might delete this
+lemma evalAtX_deg_le_degY (a : F) (f : F[X][Y]) :
+  (evalAtX a f).natDegree ≤ f.natDegree := by
+  sorry
+
+
+
+/--
+Evaluating a bivariate polynomial in the first variable `X` on a set of points resulting in a set of
+univariate polynomials in `Y`.
+-/
+def evalAtXOnASet (P : Finset F) (f : F[X][Y]) : Set (Polynomial F) :=
+ {h : Polynomial F | ∃ a ∈ P, evalAtX a f = h}
+
+/--
+The evaluation at a point of a bivariate polynomial in the second variable `Y`.
+-/
+def evalAtY (a : F) (f : F [X][Y]) : Polynomial F :=
+  Polynomial.eval (Polynomial.C a) f
+
+/--
+Evaluating a bivariate polynomial in the second variable `Y` on a set of points resulting
+in a set of univariate polynomials in `X`.
+-/
+def evalAtYOnASet (P : Finset F) (f : F[X][Y]) : Set (Polynomial F) :=
+  {h : Polynomial F | ∃ a ∈ P, evalAtY a f = h}
+
+
 
 /--
 The bivariate quotient polynomial.
@@ -251,6 +192,91 @@ Univariate quotients in `X` obtained after evaluating on a set of points.
 -/
 def setUnivQuotientX (hmn : m ≥ n) : Prop :=
   ∀ θ ∈ P, univQuotientX_deg_bd m n θ f g hmn
+
+
+
+
+
+
+
+
+---František's lemmas in the bivariate case
+
+-- lemma natDegreeBiv_lt_of_lbounded_zero_coeff {f : F[X][Y]} {deg : ℕ} [NeZero deg]
+--   (h : ∀ i, deg ≤ i → f.coeff i = 0) : f.natDegree < deg := by
+--   aesop (add unsafe [(by by_contra), (by specialize h f.natDegree)])
+
+-- /--
+-- Construct a bivariate polynomial in `X` and `Y` from a vector of coefficients,
+-- which are polynomials in `X`.
+-- -/
+-- def polynomialOfCoeffsOfBiv {deg : ℕ} [NeZero deg] (coeffs : Fin deg → F[X]) : F[X][Y] :=
+--  ⟨
+--     Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0},
+--     fun i ↦ if h : i < deg then coeffs ⟨i, h⟩ else 0,
+--     fun a ↦ by aesop (add safe (by existsi a))
+--                      (add simp [Fin.natCast_def, Nat.mod_eq_of_lt])
+--   ⟩
+
+-- @[simp]
+-- lemma natDegree_polynomialOfCoeffsOfBiv_deg_lt_deg {deg : ℕ} [NeZero deg]
+-- (coeffs : Fin deg → F[X]) :
+--   (polynomialOfCoeffsOfBiv coeffs).natDegree < deg := by
+--   aesop (add simp polynomialOfCoeffsOfBiv)
+--         (add safe apply natDegreeBiv_lt_of_lbounded_zero_coeff)
+
+-- @[simp]
+-- lemma degree_polynomialOfCoeffsOfBiv_deg_lt_deg
+--   {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
+--   (polynomialOfCoeffsOfBiv coeffs).degree < deg := by
+--   aesop (add simp [polynomialOfCoeffsOfBiv, degree_lt_iff_coeff_zero])
+
+-- @[simp]
+-- lemma coeff_polynomialOfCoeffsOfBiv_eq_coeffs {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
+--   (polynomialOfCoeffsOfBiv coeffs).coeff ∘ Fin.val = coeffs := by -- NOTE TO SELF: `liftF' coeffs`!
+--   aesop (add simp polynomialOfCoeffsOfBiv)
+
+-- lemma coeff_polynomialOfCoeffsOfBiv_eq_coeffs' {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
+--   (polynomialOfCoeffsOfBiv coeffs).coeff = fun x ↦ if h : x < deg then coeffs ⟨x, h⟩ else 0 := by
+--   aesop (add simp polynomialOfCoeffsOfBiv)
+
+-- @[simp]
+-- lemma polynomialOfCoeffsOfBiv_mem_degreeLT {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
+--   polynomialOfCoeffsOfBiv coeffs ∈ degreeLT F[X] deg := by
+--   aesop (add simp Polynomial.mem_degreeLT)
+
+
+-- @[simp]
+-- lemma polynomialOfCoeffsOfBiv_eq_zero {deg : ℕ} [NeZero deg] {coeffs : Fin deg → F[X]} :
+--   polynomialOfCoeffsOfBiv coeffs = 0 ↔ ∀ (x : ℕ) (h : x < deg), coeffs ⟨x, h⟩ = 0 := by
+--   simp [polynomialOfCoeffsOfBiv, AddMonoidAlgebra.ext_iff]
+
+-- lemma polynomialOfCoeffsBiv_coeffsOfPolynomialBiv (deg : ℕ) [NeZero deg]
+--   {f : F[X][Y]} (h : f.natDegree + 1 = deg)
+--   : polynomialOfCoeffsOfBiv (coeffsOfBivPolynomial deg f) = f := by sorry
+  -- ext x; symm
+  -- aesop (add simp [polynomialOfCoeffsOfBiv, coeffsOfBivPolynomial, coeff_polynomialOfCoeffsOfBiv_eq_coeffs'])
+  --       (add safe apply coeff_eq_zero_of_natDegree_lt)
+  --       (add safe (by omega))
+
+
+-- @[simp]
+-- lemma coeffsOfBivPolynomial_polynomialOfCoeffsOfBiv {deg : ℕ} [NeZero deg]
+--   {coeffs : Fin deg → F[X]} : coeffsOfBivPolynomial deg (polynomialOfCoeffsOfBiv coeffs) = coeffs
+--   := by
+--   ext x; symm
+--   aesop (add simp [
+--       polynomialOfCoeffsOfBiv, coeffsOfBivPolynomial, coeff_polynomialOfCoeffsOfBiv_eq_coeffs'
+--                    ])
+--         (add safe (by omega))
+
+-- @[simp]
+-- lemma support_polynomialOfBivCoeffs (deg : ℕ) [NeZero deg] {coeffs : Fin deg → F[X]} :
+--   (polynomialOfCoeffsOfBiv coeffs).support =
+--   Finset.map ⟨Fin.val, Fin.val_injective⟩ {i | coeffs i ≠ 0} := rfl
+
+
+
 
 end
 end Bivariate
