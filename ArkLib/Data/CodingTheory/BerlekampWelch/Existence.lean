bee9660e512e4ea190481d9b5f1fcedd6d6ca82d
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-06-06 13:17:49 +0200

    existence proofs and imports; cleanup variables

--- ArkLib/Data/CodingTheory/BerlekampWelch/Existence.lean
+++ ArkLib/Data/CodingTheory/BerlekampWelch/Existence.lean
@@ -3,18 +3,12 @@ Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: František Silváši, Ilia Vlasov
 -/
-import Mathlib.Algebra.Field.Basic
-import Mathlib.Algebra.Polynomial.Basic
-
-import ArkLib.Data.CodingTheory.Basic
 import ArkLib.Data.CodingTheory.BerlekampWelch.Condition
-import ArkLib.Data.CodingTheory.BerlekampWelch.ElocPoly
-import ArkLib.Data.CodingTheory.BerlekampWelch.Sorries
 
 namespace BerlekampWelch
 
 variable {α : Type} {F : Type} [Field F]
-         {n e : ℕ} {p : Polynomial F}
+         {n e k : ℕ} {p : Polynomial F}
          {ωs f : Fin n → F}
          [DecidableEq F]
 
@@ -34,6 +28,7 @@ private lemma natDegree_E
     (erase simp BerlekampWelch.elocPolyF_eq_elocPoly')
     (add safe (by omega))
 
+@[simp]
 private lemma E_ne_zero : (E ωs f p e) ≠ 0 := by
   aesop (add simp E)
 
@@ -69,7 +64,7 @@ private lemma natDegree_Q
 private lemma Q_ne_zero (hne : p ≠ 0) : Q ωs f p e ≠ 0 := by
   aesop (add simp [Q, E_ne_zero])
 
-private lemma solution_to_Q_from_Q {e k : ℕ} {ωs f : Fin n → F}
+private lemma solution_to_Q_from_Q 
   (h_p_deg : p.natDegree < k)
   (h_dist : (Δ₀(f, p.eval ∘ ωs) : ℕ) ≤ e) 
   : solution_to_Q e k (E_and_Q_to_a_solution e (E ωs f p e) (Q ωs f p e)) = Q ωs f p e := by
@@ -85,7 +80,7 @@ private lemma solution_to_Q_from_Q {e k : ℕ} {ωs f : Fin n → F}
         (add simp [Q_ne_zero, Polynomial.degree_eq_natDegree])
         (add safe (by omega))
   
-private lemma solution_to_E_from_E {e k : ℕ} {ωs f : Fin n → F}
+private lemma solution_to_E_from_E
   (h_p_deg : p.natDegree < k)
   (h_dist : (Δ₀(f, p.eval ∘ ωs) : ℕ) ≤ e) 
   : solution_to_E e k (E_and_Q_to_a_solution e (E ωs f p e) (Q ωs f p e)) = E ωs f p e := by
@@ -103,7 +98,7 @@ private lemma solution_to_E_from_E {e k : ℕ} {ωs f : Fin n → F}
     (add simp [E_ne_zero, Polynomial.degree_eq_natDegree])
     (add safe (by omega))
 
-private lemma E_and_Q_BerlekampWelch_condition {k : ℕ}
+private lemma E_and_Q_BerlekampWelch_condition
   (h_p_deg : p.natDegree < k)
   (h_dist : (Δ₀(f, p.eval ∘ ωs) : ℕ) ≤ e) 
   : BerlekampWelchCondition e k ωs f (E ωs f p e) (Q ωs f p e) := by
@@ -128,7 +123,7 @@ private lemma E_and_Q_BerlekampWelch_condition {k : ℕ}
   condition will result in the same quotient 
   `Q' \ E' = p`.
 -/
-lemma Q'_div_E'_eq_p {e k : ℕ}
+lemma Q'_div_E'_eq_p
   [NeZero n]
   {E' Q' : Polynomial F} 
   {ωs f : Fin n → F}
@@ -144,26 +139,23 @@ lemma Q'_div_E'_eq_p {e k : ℕ}
   have h_eq := E_and_Q_unique he hk_n (Q_ne_zero hp) h_Q' h_diff 
     (E_and_Q_BerlekampWelch_condition hp_deg h_ham)
     h_cond
-  simp [Q] at h_eq
-  rw [←mul_assoc, mul_comm _ (E _ _ _ _)] at h_eq 
-  aesop (add simp E_ne_zero)
+  have : Q' = E' * p := by
+    simp [Q] at h_eq
+    rw [←mul_assoc, mul_comm (E' * _)] at h_eq; simp_all
+  simp_all
 
 /-- If only up to `e` errors happened `linsolve` cannot fail to find a solution. -/
 lemma linsolve_always_some_berlekamp_welch 
-  {e k : ℕ}
   [NeZero n]
-  {ωs f : Fin n → F}
   (hp_deg: p.natDegree < k)
   (h_ham : (Δ₀(f, p.eval ∘ ωs) : ℕ) ≤ e)
-  : linsolve (BerlekampWelchMatrix e k ωs f) (Rhs e ωs f) ≠ none := by
-  intro contr
-  apply linsolve_none contr
-  exists E_and_Q_to_a_solution e (E ωs f p e) (Q ωs f p e)
-  rw [←IsBerlekampWelchSolution_def]
-  simp [
-    BerlekampWelchCondition_iff_Solution,
-    solution_to_Q_from_Q hp_deg h_ham,
-    solution_to_E_from_E hp_deg h_ham,
-    E_and_Q_BerlekampWelch_condition hp_deg h_ham]
+  : linsolve (BerlekampWelchMatrix e k ωs f) (Rhs e ωs f) ≠ none := fun contr ↦ by
+    refine linsolve_none contr ⟨E_and_Q_to_a_solution e (E ωs f p e) (Q ωs f p e), ?p₁⟩
+    rw [←IsBerlekampWelchSolution_def]
+    simp [
+      BerlekampWelchCondition_iff_Solution,
+      solution_to_Q_from_Q hp_deg h_ham,
+      solution_to_E_from_E hp_deg h_ham,
+      E_and_Q_BerlekampWelch_condition hp_deg h_ham]
 
 end BerlekampWelch
