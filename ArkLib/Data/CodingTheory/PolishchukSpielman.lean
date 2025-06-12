ac2e64bfe84edee5ddef08aac385639b7eeb36f2
Katy Hristova <katherina.hristova@gmail.com>
2025-06-12 21:06:14 +0100

    division condition fixed, moved defs, some cleanup

--- ArkLib/Data/CodingTheory/PolishchukSpielman.lean
+++ ArkLib/Data/CodingTheory/PolishchukSpielman.lean
@@ -4,14 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Katerina Hristova, František Silváši, Julian Sutherland
 -/
 
-import Mathlib.Algebra.Polynomial.Eval.Defs
-import Mathlib.Algebra.Polynomial.Bivariate
 import ArkLib.Data.CodingTheory.BivariatePoly
-import Mathlib.Data.Fintype.Defs
 
-
-open Classical
-open Polynomial
 open Polynomial.Bivariate
 
 namespace PolishchukSpielman
@@ -24,31 +18,81 @@ variable {F : Type} [Semiring F]
          (a : F)
          (f g : F[X][Y])
 
+/--
+The quotient univariate polynomial in `Y` obtained after evaluating bivariate polynomials
+`f` and `g` in `X` at a point and dividing in the ring `F[Y]`.
+-/
+def quotientPolyYAfterEvalX : Prop :=
+  ∃ p : Polynomial F, Bivariate.evalAtX a g = p * (Bivariate.evalAtX a f)
 
+/--
+Given two integers, we form a tighter degree bound on the quotient polynomial in `Y` obtained after
+evaluating in `X`.
+-/
+def quotientPolyYAfterEvalX_degBd (hmn : m ≥ n) : Prop :=
+  ∃ p, p.natDegree ≤ m - n ∧ Bivariate.evalAtX a g = p * (Bivariate.evalAtX a f)
+
+/--
+The set of univariate quotients in `Y` obtained after evaluating `X` on a given set of points.
+-/
+def setOfQuotientPolyYAfterEvalX (hmn : m ≥ n) : Prop :=
+  ∀ θ ∈ P, quotientPolyYAfterEvalX_degBd m n θ f g hmn
+
+/--
+The quotient univariate polynomial in `X` obtained after evaluating bivariate polynomials
+`f` and `g` in `Y` at a point and dividing in the ring `F[X]`.
+-/
+def quotientPolyXAfterEvalY : Prop :=
+  ∃ p : Polynomial F, Bivariate.evalAtY a g = p * (Bivariate.evalAtY a f)
+
+/--
+Given two integers, we form a tighter degree bound on the quotient polynomial in `X` obtained after
+evaluating in `Y`.
+-/
+def quotientPolyXAfterEvalY_degBd (hmn : m ≥ n) : Prop :=
+  ∃ p : Polynomial F, p.natDegree ≤ m - n ∧ Bivariate.evalAtY a g = p * (Bivariate.evalAtY a f)
+
+/--
+The set of univariate quotients in `X` obtained after evaluating `Y` on a given set of points.
+-/
+def setOfQuotientPolyXAfterEvalY (hmn : m ≥ n) : Prop :=
+  ∀ θ ∈ P, quotientPolyXAfterEvalY_degBd m n θ f g hmn
+
+/--
+Polishchuk-Spielman Lemma, existence part : If all conditions below are satisfied, then there exists
+a bivariate quotient of the given bivariate polynomials `f` and `g`. The `X` and `Y` degrees of the
+quotient satisfy given bounds too.
+-/
 lemma existence_of_bivariate_quotient [Field F] (a₁ a₂ b₁ b₂ n₁ n₂ : ℕ)
   (ha₁b₁ : b₁ ≥ a₁) (ha₂b₂ : b₂ ≥ a₂)
   (h_f_degX : a₁ ≥ Bivariate.degreeX f) (h_g_degX : b₁ ≥ Bivariate.degreeX g)
   (h_f_degY : a₂ ≥ Bivariate.degreeY f) (h_g_degY : b₂ ≥ Bivariate.degreeY g)
   (h_card_P₁ : n₁ ≥ P₁.card) (h_card_P₂ : n₂ ≥ P₂.card)
-  (h_le_1: 1 > b₁/P₁.card + b₂/P₂.card)
-  (h_quot_X : Bivariate.setUnivQuotientX b₂ a₂ P₂ f g ha₂b₂)
-  (h_quot_Y : Bivariate.setUnivQuotientY b₁ a₁ P₁ f g ha₁b₁)
-  : ∃ q : F[X][Y], g = q * f := sorry
+  (h_le_1: 1 > (b₁ : ℚ)/(P₁.card : ℚ) + (b₂ : ℚ)/(P₂.card : ℚ))
+  (h_quot_X : setOfQuotientPolyXAfterEvalY b₂ a₂ P₂ f g ha₂b₂)
+  (h_quot_Y : setOfQuotientPolyYAfterEvalX b₁ a₁ P₁ f g ha₁b₁)
+  : ∃ q : F[X][Y], g = q * f
+    ∧ Bivariate.degreeX q ≤ b₁ - a₁ ∧ Bivariate.degreeY q ≤ b₂ - a₂ := sorry
 
-
+/--
+Polishchuk-Spielman Lemma, properties of the bivariate quotient: If the bivariate quotient exists,
+then it is consistent on a certain number of points with the univariate quotients
+obtained after evaluating in `X` and `Y` respectively.
+-/
 lemma properties_of_bivariate_quotient [Field F] (a₁ a₂ b₁ b₂ n₁ n₂ : ℕ)
   (ha₁b₁ : b₁ ≥ a₁) (ha₂b₂ : b₂ ≥ a₂) (q : F[X][Y])
   (h_f_degX : a₁ ≥ Bivariate.degreeX f) (h_g_degX : b₁ ≥ Bivariate.degreeX g)
   (h_f_degY : a₂ ≥ Bivariate.degreeY f) (h_g_degY : b₂ ≥ Bivariate.degreeY g)
   (h_card_P₁ : n₁ ≥ P₁.card) (h_card_P₂ : n₂ ≥ P₂.card)
-  (h_le_1: 1 > b₁/P₁.card + b₂/P₂.card)
-  (h_quot_X : Bivariate.setUnivQuotientX b₂ a₂ P₂ f g ha₂b₂)
-  (h_quot_Y : Bivariate.setUnivQuotientY b₁ a₁ P₁ f g ha₁b₁)
+  (h_le_1: 1 > (b₁ : ℚ)/(P₁.card : ℚ) + (b₂ : ℚ) /(P₂.card : ℚ))
+  (h_quot_X : setOfQuotientPolyXAfterEvalY b₂ a₂ P₂ f g ha₂b₂)
+  (h_quot_Y : setOfQuotientPolyYAfterEvalX b₁ a₁ P₁ f g ha₁b₁)
   (h_quot_XY : g = q * f) :
   ∃ P₃ : Finset F, P₃.card ≥ n₁ - a₁
-  ∧ Bivariate.setUnivQuotientY b₁ a₁ P₃ f g ha₁b₁
-  ∧ ∃ P₄ : Finset F, P₄.card ≥ n₂ - a₂ ∧ Bivariate.setUnivQuotientY b₂ a₂ P₄ f g ha₂b₂
+  ∧ setOfQuotientPolyYAfterEvalX b₁ a₁ P₃ f g ha₁b₁
+  ∧ ∃ P₄ : Finset F, P₄.card ≥ n₂ - a₂ ∧ setOfQuotientPolyYAfterEvalX b₂ a₂ P₄ f g ha₂b₂
   := by sorry
+
 
 end
 end PolishchukSpielman
