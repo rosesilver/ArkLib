a9ab39d2f87dc80a53cdb8b919212aa0b3043aa2
Rose Silver <silver.r@northeastern.edu>
2025-06-18 16:34:32 -0400

    init Shout/Basic.lean

---
+++ ArkLib/ProofSystem/Shout/Basic.lean
@@ -0,0 +1,11 @@
+/-
+Copyright (c) 2024 ArkLib Contributors. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Rose Silver
+-/
+
+import ArkLib.OracleReduction.Security.Basic
+import ArkLib.ProofSystem.ConstraintSystem.R1CS
+import ArkLib.Data.MvPolynomial.Multilinear
+import ArkLib.ProofSystem.Sumcheck.Spec.General
+-- import ArkLib.ProofSystem.Components
