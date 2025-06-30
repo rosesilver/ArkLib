139f81065d078971d65824bb45ac2990b50fb574
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-06-30 14:45:12 +0200

    add coauthor:

--- ArkLib/Data/CodingTheory/JohnsonBound.lean
+++ ArkLib/Data/CodingTheory/JohnsonBound.lean
@@ -1,6 +1,6 @@
 /- Copyright (c) 2024-2025 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Ilia Vlasov 
+Authors: Ilia Vlasov, František Silváši
 -/
 import ArkLib.Data.CodingTheory.JohnsonBound.Lemmas
 
