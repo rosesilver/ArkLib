84cad6fd6265578744268e253b4385517767667e
Chung Thai Nguyen <chung.thai.ng@gmail.com>
2025-06-30 22:03:14 +0700

    feat: novel polynomial basis (#77)
    * feat: blueprint for binary tower fields
    
    * feat: novel polynomial basis
    
    ---------
    
    Co-authored-by: Quang Dao <qvd@andrew.cmu.edu>

--- ArkLib.lean
+++ ArkLib.lean
@@ -23,6 +23,8 @@ import ArkLib.Data.CodingTheory.ProximityGap
 import ArkLib.Data.CodingTheory.ReedMuller
 import ArkLib.Data.CodingTheory.ReedSolomon
 import ArkLib.Data.EllipticCurve.BN254
+import ArkLib.Data.FieldTheory.BinaryField.AdditiveNTT.NovelPolynomialBasis
+import ArkLib.Data.FieldTheory.BinaryField.AdditiveNTT.Prelude
 import ArkLib.Data.FieldTheory.BinaryField.Tower.Basic
 import ArkLib.Data.FieldTheory.BinaryField.Tower.Impl
 import ArkLib.Data.FieldTheory.BinaryField.Tower.Prelude
