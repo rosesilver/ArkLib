04e586168a8fd0fbd269626570b79a7c5f9e9f64
Rose Silver <silver.r@northeastern.edu>
2025-07-07 17:24:01 -0400

    wip: refactor Shout protocol setup, add Spec namespace, public params, and oracle interface instance for input oracles

--- ArkLib/ProofSystem/Shout/Basic.lean
+++ ArkLib/ProofSystem/Shout/Basic.lean
@@ -1,5 +1,5 @@
 /-
-Copyright (c) 2024 ArkLib Contributors. All rights reserved.
+Copyright (c) 2025 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rose Silver
 -/
@@ -12,56 +12,59 @@ import ArkLib.ProofSystem.Sumcheck.Spec.General
 
 namespace Shout
 
--- number of registers
-variable (K : ℕ) --assuming K is a power of 2
+-- Public Parameters
+structure PublicParams where
+  logK : ℕ
+  logT : ℕ
+  F : Type
 
--- log K
-variable (logK : ℕ)
+namespace PublicParams
 
---number of cycles
-variable (T : ℕ) --assuming T is a power of 2
+--variable (pp : PublicParams)
+-- the read-accesses vector ra, read-values vector rv, static lookup table Val
+structure SetUp (pp : PublicParams) where
+  ra : MlPoly pp.F (pp.logK + pp.logT)
+  rv : MlPoly pp.F pp.logT
+  val : MlPoly pp.F pp.logK
+end PublicParams
 
--- log T
-variable (logT : ℕ)
+/-
+variable (ra : MlPoly pp.F (pp.logK + pp.logT))
+variable (rv : MlPoly pp.F pp.logT)
+variable (Val : MlPoly pp.F pp.logK)
+-/
+-- number of variables in polynomial ra
+def ra_num_vars : ℕ := pp.logK + pp.logT
 
--- field
-variable (F : Type) --TODO: make a field?
-
--- The set of register indices
-def Registers : Type := Fin K
-
--- The set of cycle indices
-def Cycles : Type := Fin T
-
--- the read-accesses vector
---variable (ra : Registers logK × Cycles logT → F)
-variable (ra : Registers (2 ^ logK) × Cycles (2 ^ logT) → F)
-
--- the read-values vector
-variable (rv : Cycles (2 ^ logT) → F)
-
--- the static lookup table
-variable (Val : Registers (2 ^ logK) → F)
-
--- TODO: change var name to be more descriptive
--- polynomial len
-def n : ℕ := logK + logT
--- def n (logK : ℕ) (logT : ℕ) : ℕ := logK + logT
+-- The set of register and cycle indices
+def Registers (K : ℕ): Type := Fin K
+def Cycles (T : ℕ): Type := Fin T
 
 -- the single-round protocol specification
-def pSpec : ProtocolSpec 1 := ![(.P_to_V, MlPoly F (n logK logT))]
+def pSpec : ProtocolSpec 1 := ![(.P_to_V, MlPoly pp.F (ra_num_vars pp))]
 
 -- the input statement type
 def StmtIn : Type := Unit
 
 -- the input witness type
-def WitIn : Type := (Registers (2 ^ logK) × Cycles (2 ^ logT) → F) × (Cycles (2 ^ logT) → F)
-
+def WitIn : Type :=
+  (Registers (2 ^ pp.logK) × Cycles (2 ^ pp.logT) → pp.F) × (Cycles (2 ^ pp.logT) → pp.F)
 
 -- the input oracle statement type
--- TODO
+def OStmtIn : Fin 1 → Type := fun _ => (Registers (2 ^ pp.logK) → pp.F)
 
-def OStmtIn : Fin 1 → Type := fun _ => (Registers (2 ^ logK) → F)
+-- the oracle interface
+instance : ∀ i, OracleInterface (OStmtIn pp i) :=
+  fun _ => {
+    Query := Fin (2 ^ pp.logK)
+    Response := pp.F
+    oracle := fun Val k => Val k
+  }
+
+-- the relation
+
+end Spec
+
 
 variable {ι : Type} (oSpec : OracleSpec ι) (StmtIn WitIn StmtOut WitOut : Type)
 variable {ιₛ : Type} (OStmtIn : ιₛ → Type) [∀ i, OracleInterface (OStmtIn i)]
@@ -71,8 +74,9 @@ section OracleReduction
 
 @[inline, specialize]
 def oracleProver : OracleProver (pSpec logK logT F) oSpec StmtIn WitIn StmtOut WitOut OStmtIn OStmtOut where
-  --PrvState | 0 => sorry
-  PrvState := sorry
+  PrvState := fun i => match i with
+   | 0 => sorry
+   | 1 => sorry
   input := sorry
   sendMessage := sorry
   receiveChallenge := sorry
@@ -80,3 +84,23 @@ def oracleProver : OracleProver (pSpec logK logT F) oSpec StmtIn WitIn StmtOut W
 
 end OracleReduction
 end Shout
+
+/- SCRATCH WORK
+def Regs (logK : ℕ): Type := Fin (2^logK) → Fin (logK) → Bool
+def reg {logK : ℕ} (var : Fin (logK)): Regs (logK) := sorry
+def Regs {logK : ℕ} : Type := Fin (2^logK) → Fin (logK) → Bool
+def Regs : Type := ∀ (logK : ℕ), Fin (2^logK) → Fin logK → Bool
+def reg (logK : ℕ) (myNum : Fin (2 ^ logK)) (myBit : Fin (logK)) : Bool :=
+    ((myNum.val >>> myBit.val) % 2) = 1
+
+    def Regs (logK : ℕ) : Type := Fin (2^logK) → Fin logK → Bool
+def reg (logK : ℕ) (myNum : Fin (2 ^ logK)) (myBit : Fin (logK)) : Bool :=
+  ((myNum.val >>> myBit.val) % 2) = 1
+myNum
+myBit
+
+variable (rv : Cycles (2 ^ logT) → F)
+variable (ra : Registers logK × Cycles logT → F)
+variable (Val : Registers (2 ^ logK) → F)
+variable (ra : Registers (2 ^ logK) × Cycles (2 ^ logT) → F)
+-/
