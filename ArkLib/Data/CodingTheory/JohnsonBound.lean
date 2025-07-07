c07c998d3fe4a20883161d351f8d912063513ebe
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-07-07 12:48:12 +0200

    shuffle imports a little bit more

--- ArkLib/Data/CodingTheory/JohnsonBound.lean
+++
@@ -1,4 +0,0 @@
-import ArkLib.Data.CodingTheory.JohnsonBound.Basic
-import ArkLib.Data.CodingTheory.JohnsonBound.Choose2
-import ArkLib.Data.CodingTheory.JohnsonBound.Expectations
-import ArkLib.Data.CodingTheory.JohnsonBound.Lemmas
